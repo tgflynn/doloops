6a0fc13960c67a8606ffeaf13a3f29467cbadfca
Timothy G. Flynn <tgflynn@users.noreply.github.com>
2025-05-25 10:44:05 -0400

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # doloops
-A common lisp nested loop macro
+A Common Lisp nested loop macro
+
+This is a very simple project to illustrate a method for writing a
+Common Lisp macro for nested loops.  It works in a similar manner to
+dotimes but accepts multiple variable/count specifications and
+implements a loop that nests them all.
+
+It seems to me that this technique could be useful in reducing
+cyclomatic complexity in software.
+
+The reason for doing this example implmenttation in CL is that the
+macro system allows it to be done without modifying the language
+itself. 
+
+The name of the main function is doloops with associated test function
+dotestloops. 
+
+Also included is a version called doexp that doesn't use macros but
+calls a lambda function with a variable counter instead.  This version
+can be implemented in any language that supports arrays and
+higher-order functions.
+
+I have only tested this code with sbcl on Linux.
---
+++ doloops.lisp
@@ -0,0 +1,109 @@
+;;(require :sb-cltl2)
+
+(defun expand (form)
+  (pprint (sb-cltl2:macroexpand-all form)))
+
+(defun clamp (val min max)
+  (cond
+    ((< val min) min)
+    ((> val max) max)
+    (t val)))
+
+(defun clampmin (val min)
+  (if (< val min) min
+      val))
+
+(defun incmodf (counters i start base)
+  (let ((newval (clampmin (mod (1+ (elt counters i)) (clampmin base 1)) start)))
+    (setf (elt counters i) newval)
+    (= newval start)))
+
+(defun counter-inc (counters finvals &key initvals)
+  (when (not initvals)
+    (setf initvals (make-array (array-dimensions finvals))))
+  (let ((n (length counters)))
+    ;;(format t "n: ~a~%" n)
+    (do ((i (- n 1)))
+        ((< i 0) t)
+      ;;(format t "i: ~a counters: ~a~%" i counters)
+      (if (incmodf counters i (elt initvals i) (elt finvals i))
+          (decf i)
+          (return nil)))))
+
+(defun doexp (vars fun)
+  (let* (
+         (n (length vars))
+         (initvals (make-array n ::initial-contents (map 'list #'car vars)))
+         (finvals (make-array n ::initial-contents (map 'list #'cadr vars)))
+         (done nil)
+         (dostep #'(lambda (x) (let ((res (counter-inc x finvals :initvals initvals)))
+                                 (when res (setf done t)))
+                     x)))
+    (do ((counters (copy-seq initvals) (funcall dostep counters)))
+        (done)
+      (funcall fun counters))))
+
+(defun doexpl (vars fun)
+  (let* (
+         (n (length vars))
+         (initvals (make-array n ::initial-contents (map 'list #'car vars)))
+         (finvals (make-array n ::initial-contents (map 'list #'cadr vars)))
+         (done nil)
+         (dostep #'(lambda (x) (let ((res (counter-inc x finvals :initvals initvals)))
+                                 (when res (setf done t)))
+                     x)))
+    (do ((counters (copy-seq initvals) (funcall dostep counters)))
+        (done)
+      (funcall fun (coerce counters 'list)))))
+
+
+(defmacro doloops (vardefs &body body)
+  (let ((vars (mapcar #'car vardefs)))
+    `(let* ((n (length ',vardefs))
+            (initvals (make-array n ::initial-contents (mapcar #'cadr ',vardefs)))
+            (finvals (make-array n ::initial-contents (mapcar #'caddr ',vardefs)))
+            (done nil)
+            (dostep #'(lambda (x) (let ((res (counter-inc x finvals :initvals initvals)))
+                                    (when res (setf done t)))
+                        x)))
+       (do ((counters (copy-seq initvals) (funcall dostep counters)))
+           (done)
+         (destructuring-bind ,vars (coerce counters 'list)
+           ,@body)))))
+
+(defmacro arefl (array arridx)
+  `(apply #'aref (append (list ,array) ,arridx)))
+
+(defmacro alindex (array arridx)
+  `(apply #'array-row-major-index (append (list ,array) ,arridx)))
+
+
+(defun copy-array (a)
+  (declare (optimize (debug 3)))
+  (let* ((adims (array-dimensions a))
+         (aranges (mapcar #'(lambda (x) (list 0 x)) (array-dimensions a)))
+         (acopy (make-array adims
+                            :element-type (array-element-type a))))
+        (doexp aranges #'(lambda (adim)
+                     (let ((arridx (coerce adim 'list)))
+                       (setf (arefl acopy arridx) (arefl a arridx))
+                       )))
+    acopy))
+
+
+(defun dotestloops ()
+  (doloops ((i 0 5) (j 0 5))
+    (format t "i=~a j=~a~%" i j)))
+
+(defun dotestexp ()
+  (doexp '((0 5) (0 5))
+         #'(lambda (x) (format t "x: ~a~%" x))))
+
+(defun dotestexpl ()
+  (doexpl '((0 5) (0 5))
+         #'(lambda (x) (format t "x: ~a~%" x))))
+
+(defun dotestexpl-stuck ()
+  (doexpl '((0 5) (5 5) (0 0))
+         #'(lambda (x) (format t "x: ~a~%" x))))
+
